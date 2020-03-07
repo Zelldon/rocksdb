382ecd84fa90510890f8d16596c4c26e6ea87f45
Christopher Zell <zelldon91@googlemail.com>
2020-03-07 23:39:28 +0100

    add statistics and run count - add multiple cfgs

--- examples/optimistic_transaction_example.cc
+++ examples/optimistic_transaction_example.cc
@@ -13,6 +13,9 @@
 #include "rocksdb/utilities/optimistic_transaction_db.h"
 #include "rocksdb/table.h"
 
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+
 using namespace ROCKSDB_NAMESPACE;
 
 std::string kDBPath = "/tmp/rocksdb_transaction_example";
@@ -21,32 +24,47 @@ void createColumnFamilies(const int columnFamilyCount, const ColumnFamilyOptions
                           std::vector<ColumnFamilyDescriptor> &column_families);
 
 int main() {
+
     DBOptions dbOptions;
     dbOptions.create_if_missing = true;
     dbOptions.create_missing_column_families = true;
-    dbOptions.db_write_buffer_size = 512 * 1024 * 1024;
 
-    std::cout << "DBOptions: " << std::endl;
-    std::cout << "\t" << "db_write_buffer_size: " << dbOptions.db_write_buffer_size << std::endl;
-    std::cout << "\t" << "max_open_files: " << dbOptions.max_open_files << std::endl;
-    std::cout << "\t" << "write_buffer_manager: " << dbOptions.write_buffer_manager << std::endl;
+    dbOptions.statistics = CreateDBStatistics();
+    dbOptions.dump_malloc_stats = true;
 
-
+    // index and bloom filter
+    // table option
     BlockBasedTableOptions basedTableOptions = BlockBasedTableOptions();
-    basedTableOptions.block_size = 32 * 1024;
-
-    auto cache = NewLRUCache(512 * 1024 * 1024);
+    basedTableOptions.block_size = 32 * 1024 * 1024;
+    auto cache = NewLRUCache(64 * 1024 * 1024);
     basedTableOptions.block_cache = cache;
     basedTableOptions.cache_index_and_filter_blocks = true;
+    basedTableOptions.pin_l0_filter_and_index_blocks_in_cache = true;
+
+    // mem table
+//    WriteBufferManager* manager = new WriteBufferManager((2 * 1024  * 1024 * 1023) + 1023, cache);
+//    dbOptions.write_buffer_manager.reset(manager);
+
+
+    // THIS WORKS DEF ->
+    // 64 mb def by one column took ~ 30 s
+    // 128 mb def by one column took 15
+    // 2 gig by 50 column took took 14
+    // these numbers above with small keys 32 kb
+    // with larger keys there where no diff?
+    dbOptions.db_write_buffer_size = 0; // (2 * 1024  * 1024 * 1023) + 1023;
+    // necessary otherwise our benchmark fails
+    dbOptions.max_open_files = 512;
 
     const int columnFamilyCount = 50;
     ColumnFamilyOptions columnFamilyOptions = ColumnFamilyOptions();
-    columnFamilyOptions.optimize_filters_for_hits = true;
-    columnFamilyOptions.table_factory.reset(NewBlockBasedTableFactory(basedTableOptions));
+    columnFamilyOptions.max_write_buffer_number = 1;
+    columnFamilyOptions.write_buffer_size = 32 * 1024 * 1024;
 
-    std::cout << "ColumnFamilyOptions: " << std::endl;
-    std::cout << "\t" << "write_buffer_size: " << columnFamilyOptions.write_buffer_size << std::endl;
-    std::cout << "\t" << "arena_block_size: " << columnFamilyOptions.arena_block_size << std::endl;
+    columnFamilyOptions.max_write_buffer_size_to_maintain = 0;
+    columnFamilyOptions.min_write_buffer_number_to_merge = 0;
+//    columnFamilyOptions.optimize_filters_for_hits = true;
+//    columnFamilyOptions.table_factory.reset(NewBlockBasedTableFactory(basedTableOptions));
 
     std::vector<ColumnFamilyDescriptor> column_families;
     createColumnFamilies(columnFamilyCount, columnFamilyOptions, column_families);
@@ -69,13 +87,15 @@ int main() {
     //
     ////////////////////////////////////////////////////////
 
-    const int transactionCount = 100000;
+    const int transactionCount = 10000;
 
     std::cout << "~ Start Benchmark ~" << std::endl;
     std::cout << "Column family count: " << columnFamilyCount << std::endl;
     std::cout << "Transaction count: " << transactionCount << std::endl;
 
     const clock_t startTime = clock();
+    auto key = std::string(1024 * 1024, 'a');
+    clock_t iterationTime = clock();
     for (int i = 0; i < transactionCount; ++i) {
 
         // Start a transaction
@@ -84,12 +104,22 @@ int main() {
 
         // Write a key in this transaction
         ColumnFamilyHandle *&family = handles.at(i % handles.size());
-        s = txn->Put(family, std::string(1024 * ( (i % 1024) + 1), 'a'), "abc");
+        s = txn->Put(family, key + std::to_string(i), "abc");
         assert(s.ok());
 
         // Commit transaction
         s = txn->Commit();
-        assert(s.ok());
+        if (!s.ok())
+        {
+            std::cout << s.ToString() << std::endl;
+        }
+
+        if (i % 100 == 0)
+        {
+            const clock_t currentClock = clock();
+            std::cout << "Transaction " << i << " took " << double(currentClock - iterationTime) / CLOCKS_PER_SEC << " secs since last iteration." << std::endl;
+            iterationTime = currentClock;
+        }
         delete txn;
     }
 
@@ -98,6 +128,8 @@ int main() {
 
     std::cout << "~ Finished OptimisticDB benchmark ~" << std::endl;
     std::cout << " Benchmark took: " << double(endTime - startTime) / CLOCKS_PER_SEC << " sec" << std::endl;
+
+    std::cout << " statistics: " << dbOptions.statistics.get()->ToString() << std::endl;
 
     // Cleanup
     delete txn_db;
